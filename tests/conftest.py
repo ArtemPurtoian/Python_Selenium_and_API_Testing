c42087bb97d7907df2a192fdeff383d269462f5f
unknown <artemios.od@gmail.com>
2023-09-12 15:36:52 +0300

    added fixtures for encapsulation

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,9 @@ from utilities.driver_factory import create_drivers
 from utilities.config_reader import ReadConfig
 from contextlib import suppress
 import allure
-import inspect
+from page_objects.login_page import LoginPage
+from page_objects.home_page import HomePage
+from page_objects.messages_page import MessagePage
 
 
 @pytest.fixture
@@ -28,8 +30,38 @@ def pytest_runtest_makereport(item):
     return rep
 
 
-def allure_auto_step(cls):
-    for name, method in inspect.getmembers(cls, inspect.isfunction):
-        if name != "__init__":
-            setattr(cls, name, allure.step(method))
-    return cls
+@pytest.fixture
+def open_login_page(initialize_driver):
+    email = ReadConfig.get_user_email()
+    password = ReadConfig.get_user_password()
+    driver = initialize_driver
+    (LoginPage(driver).set_email(email).set_password(password).
+     click_login_button())
+    return HomePage(driver)
+
+
+@pytest.fixture
+def open_messages_page(initialize_driver):
+    email = ReadConfig.get_user_email()
+    password = ReadConfig.get_user_password()
+    driver = initialize_driver
+    (LoginPage(driver).set_email(email).set_password(password).
+     click_login_button())
+    (MessagePage(driver).click_message_tab_button().
+     click_notification_not_now_button())
+    return MessagePage(driver)
+
+
+@pytest.fixture
+def search_user(open_login_page):
+    searched_user = \
+        (open_login_page.click_search_button().
+         input_page_name_to_search(ReadConfig.get_page_name_to_search()).
+         click_desired_page())
+    return searched_user
+
+
+@pytest.fixture
+def open_user_page(open_login_page):
+    user_page = open_login_page.open_user_profile()
+    return user_page
